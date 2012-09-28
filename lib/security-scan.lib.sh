8cabd0ebc613bbf028c85a6acbeda8a00924248c
Peter Chester <peter@tri.be>
2012-09-27 19:57:54 -0700

    Update security script to support external config file.
    Signed-off-by: Peter Chester <peter@tri.be>

---
+++ lib/security-scan.lib.sh
@@ -0,0 +1,157 @@
+#!/bin/bash
+
+## WordPress Security Scan Script
+## Copyright by Peter Chester of Modern Tribe, Inc.
+## Permission to copy and modify is granted under the GPL2 license
+## Last revised 2012-09-27
+##
+## DESCRIPTION:
+##
+## * Uses SVN or GIT to make sure no rogue files exist in an install
+## * Removes PHP files from blogs.dir and uploads
+## * Removes HTML/HTM files from blogs.dir and uploads
+## * Updates file and folder permissions and ownership
+##
+## * Bonus: this effectively acts as a deployment system. Every time you run
+##   the script, your version controlled code is deployed.
+##
+## USAGE:
+##
+## sudo bash security-scan.sh
+##
+## It is recommended that you set this up with a cron so that the scan is
+## regularly performed. Ideally the script lives outside of your webroot
+## directory. The cron would looks something like this:
+##
+## */5 * * * * bash /path/to/security-scan.sh
+##
+## This will run the cron once every 5 minutes.
+##
+## TODO:
+##
+## * Scan nginx/htaccess files for security
+## * Update permissions scan to ignore files with correct perms
+##
+
+# Check for required variables.
+REQUIRED_VARS=( "ROOT_DIR" "WRITEABLE_DIRS" "WRITEABLE_FILES" "WEB_OWNER" "CODE_OWNER" )
+for REQUIRED_VAR in ${REQUIRED_VARS[@]}
+do
+	# Test that vars are set
+	REQUIRED_VAR_TEST=$(eval echo "\$${REQUIRED_VAR}")
+	if [ -z "${REQUIRED_VAR_TEST+x}" ]
+	then
+		echo "ERROR: '$REQUIRED_VAR' is required."
+		exit
+	fi
+done
+
+# Change directories to the webroot (ROOT_DIR).
+if [ -d $ROOT_DIR ]
+then
+	cd "$ROOT_DIR"
+else
+	echo "Error: the directory specified in ROOT_DIR does not exist."
+	echo "$ROOT_DIR"
+	exit
+fi
+
+echo "Running scan on $PWD..."
+
+# SVN Scan
+if [ -d ".svn" ]
+then
+	echo "Detected SVN environment"
+
+	echo "Cleaning SVN..."
+	svn cleanup
+
+	echo "Updating SVN..."
+	svn up
+
+	echo "Reverting SVN..."
+	svn revert -R .
+
+	echo "Pruning unversioned files..."
+	rm -rf `svn status . | grep '^?' | awk '{ print $2 }' | xargs`
+fi
+
+# GIT Scan
+if [ -d ".git" ]
+then
+	echo "Detected GIT environment"
+
+	if [ -f ".git/index.lock" ]
+	then
+		echo "GIT appears to be LOCKED. Deleteing .git/index.lock."
+		rm -rf ".git/index.lock"
+	fi
+
+	echo "Resetting GIT..."
+	git reset --hard HEAD
+
+	echo "Pulling GIT..."
+	git pull
+
+	echo "Attempting to update Submodules..."
+	git submodule init
+	git submodule update
+
+	echo "Cleaning GIT..."
+	git clean -df
+fi
+
+# Adjusting directory permissions
+if [ -z ${DIR_PERM} ]; then DIR_PERM="755"; fi
+echo "Updating file permissions on all directories to be $DIR_PERM"
+find "$PWD" -type d -exec chmod "$DIR_PERM" {} \;
+
+# Adjust file permissions
+if [ -z ${FILE_PERM} ]; then FILE_PERM="644"; fi
+echo "Updating file permissions on all files to be $FILE_PERM"
+find "$PWD" -type f -exec chmod "$FILE_PERM" {} \;
+
+# Update all file owners
+echo "Update all files to be owned by $CODE_OWNER"
+chown $CODE_OWNER "$PWD" -R
+
+# Process writable directories
+for DIR in ${WRITEABLE_DIRS[@]}
+do
+	# Scan directory
+	if [ -d "$DIR" ]
+	then
+		echo "Scanning and removing PHP files from $DIR..."
+		find "$DIR" -name '*php'
+		find "$DIR" -name '*php' | xargs rm -rf
+
+		echo "Scanning and removing HTML files from $DIR..."
+		find "$DIR" -name '*html'
+		find "$DIR" -name '*htm'
+		find "$DIR" -name '*html' | xargs rm -rf
+		find "$DIR" -name '*htm' | xargs rm -rf
+
+		echo "Updating file ownership to $WEB_OWNER $DIR..."
+		chown $WEB_OWNER $DIR -R
+	else
+		echo "Error: $PWD/$DIR not found."
+	fi
+done
+
+# Process writable files
+for WFILE in ${WRITEABLE_FILES[@]}
+do
+	# Scan file
+	if [ -f "$WFILE" ]
+	then
+		echo "Updating file ownership to $WEB_OWNER $WFILE..."
+		chown $WEB_OWNER $WFILE
+
+		echo "Updating file permissions to 755 on $WFILE..."
+		chmod 755 $WFILE
+	else
+		echo "Error: $PWD/$WFILE not found."
+	fi
+done
+
+echo "Scan complete!"
--- security-scan.sh
+++ security-scan.sh
@@ -1,114 +1,36 @@
 #!/bin/bash
 
 ## WordPress Security Scan Script
-## Copyright 2012 by Peter Chester of Modern Tribe, Inc.
-## Permission to copy and modify is granted under the GPL2 license
-## Last revised 7/6/2012
+## Executable Config File
 ##
-## DESCRIPTION:
+## Copy and modify this file with the following variables and execute this
+## file manually or via cron to run the script. Ideally you'll want to run
+## this as root using 'sudo'.
 ##
-## * Uses SVN or GIT to make sure no rogue files exist in an install
-## * Removes PHP files from blogs.dir and uploads
-## * Removes HTML/HTM files from blogs.dir and uploads
-## * Bonus: this effectively acts as a deployment system. Every time you run
-##   the script, your version controlled code is deployed.
-##
-## USAGE:
-##
-## sh security-scan.sh /path/to/WordPress/install/
-##
-## It is recommended that you set this up with a cron so that the scan is
-## regularly performed. Ideally the script lives outside of your webroot
-## directory. The cron would looks something like this:
-##
-## 0 * * * * sh /path/to/security-scan.sh /path/to/WordPress/install/
-##
-## This will run the cron once every hour on the hour.
-##
-## TODO:
-##
-## * Scan nginx/htaccess files for security
-## * Update file permissions
+## This file can be placed anywhere on your server as long as the references
+## back to the ROOT_DIR and the library (last line) are absolute.
 ##
 
-# Pass an optional argment to specify a path.
-if [ -d $1 ]
-then
-	cd "$1"
-else
-	echo "Error: the directory specified does not exist."
-	exit
-fi
+# Root web directory
+ROOT_DIR="/absolute/path/to/the/web/root/folder"
 
-echo "Running scan on $PWD..."
+# List of all PHP writeable directories
+WRITEABLE_DIRS=( "relative/path1" "relative/path2" "etc..." )
 
-# SVN
-if [ -d ".svn" ]
-then
-	echo "Detected SVN environment"
+# List of all PHP writeable files
+WRITEABLE_FILES=( "relative/file/path1.txt" "realtive/file/path2.txt" "etc..." )
 
-	echo "Cleaning SVN..."
-	svn cleanup
+# PHP writeable user:group - this should be whatever the user:group is that PHP is using
+WEB_OWNER="www-data:www-data"
 
-	echo "Updating SVN..."
-	svn up
+# Private code user:group
+CODE_OWNER="www:www"
 
-	echo "Reverting SVN..."
-	svn revert -R .
+# Permissions for directories (defaults to 755)
+DIR_PERM="755"
 
-	echo "Pruning unversioned files..."
-	rm -rf `svn status . | grep '^?' | awk '{ print $2 }' | xargs`
-fi
+# Permissions for files (defaults to 644)
+FILE_PERM="644"
 
-# GIT
-if [ -d ".git" ]
-then
-	echo "Detected GIT environment"
-
-	echo "Resetting GIT..."
-	git reset --hard HEAD
-
-	echo "Pulling GIT..."
-	git pull
-
-	echo "Attempting to update Submodules..."
-	git submodule init
-	git submodule update
-
-	echo "Cleaning GIT..."
-	git clean -df
-fi
-
-# Scan wp-content/uploads
-if [ -d "wp-content/uploads" ]
-then
-	echo "Scanning and removing PHP files from wp-content/uploads..."
-	find "wp-content/uploads" -name '*php'
-	find "wp-content/uploads" -name '*php' | xargs rm -rf
-
-	echo "Scanning and removing HTML files from wp-content/uploads..."
-	find "wp-content/uploads" -name '*html'
-	find "wp-content/uploads" -name '*htm'
-	find "wp-content/uploads" -name '*html' | xargs rm -rf
-	find "wp-content/uploads" -name '*htm' | xargs rm -rf
-else
-	echo "Error:$PWD/wp-content/uploads not found."
-fi
-
-# Scan wp-content/blogs.dir
-if [ -d "wp-content/blogs.dir" ]
-then
-	echo "Scanning and removing PHP files from wp-content/blogs.dir..."
-	find "wp-content/blogs.dir" -name '*php'
-	find "wp-content/blogs.dir" -name '*php' | xargs rm -rf
-
-	echo "Scanning and removing HTML files from wp-content/blogs.dir..."
-	find "wp-content/blogs.dir" -name '*html'
-	find "wp-content/blogs.dir" -name '*htm'
-	find "wp-content/blogs.dir" -name '*html' | xargs rm -rf
-	find "wp-content/blogs.dir" -name '*htm' | xargs rm -rf
-else
-	echo "Error:$PWD/wp-content/blogs.dir not found."
-fi
-
-echo "Scan complete!"
+# Include the actual script (use the full path)
+source /absolute/path/to/this/folder/lib/security-scan.lib.sh
